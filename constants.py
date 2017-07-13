846fd57ed9f959e2e59b35122c9ecfb69fb7ac71
Yogesh Kumar <yogesh.biebz@gmail.com>
2017-07-13 15:53:36 +0530

    first commit in instabot project

---
+++ constants.py
@@ -0,0 +1,5 @@
+# File to introduce constants
+
+APP_ACCESS_TOKEN='1690315985.1597e9b.2eb4413fd895415da41e8e299be36733'          # Access token genrated from developer account
+BASE_URL='https://api.instagram.com/v1/'            # Base URl
+insta_username = ""
